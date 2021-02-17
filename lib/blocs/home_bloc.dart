65db60a5ea4e41c47ed35c4ca4c229d79f9de48d
Oleksandr <autour89@outlook.com>
2021-02-17 09:03:12 +0200

    basic calc layout

--- lib/blocs/home_bloc.dart
+++ lib/blocs/home_bloc.dart
@@ -1,1 +1,5 @@
+import 'package:flutter/cupertino.dart';
 
+class TetrisBloc extends ChangeNotifier {
+  TetrisBloc() {}
+}
--- lib/screens/home_vscreen.dart
+++ lib/screens/home_vscreen.dart
@@ -9,39 +9,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  String _inputText;
 
-  void _incrementCounter() async {
+  void _incrementCounter(String val) {
     setState(() {
-      _counter++;
+      _inputText = _inputText + val;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var text = '|';
+    var model = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0',
+      '-',
+      '+',
+      '*',
+      '/',
+      '='
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many2 times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: SafeArea(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+              SizedBox(
+                  width: double.infinity,
+                  height: 100,
+                  child: Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            text,
+                            style: TextStyle(fontSize: 22),
+                          )),
+                      color: Colors.teal[100])),
+              Expanded(
+                  child: GridView.count(
+                      crossAxisCount: 3,
+                      padding: EdgeInsets.all(10),
+                      primary: false,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      children: [
+                    ...model.map((e) => Container(
+                        padding: const EdgeInsets.all(8),
+                        child: TextButton(
+                          child: Text(
+                            e,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          onPressed: () {},
+                        ),
+                        color: Colors.teal[100]))
+                  ]))
+            ])));
   }
 }
