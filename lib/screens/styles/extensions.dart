876507f0c8270ef9e04f134a420af79363e1888f
Oleksandr Shastun <Oleksandr.Shastun@3shape.com>
2021-03-02 07:31:17 +0200

    a bit simplified bloc structure

---
+++ lib/screens/styles/extensions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+extension Home on Widget {
+  Widget withBox(
+      {BoxDecoration boxDecoration, double padding = 3, double margin = 3}) {
+    return Container(
+      margin: EdgeInsets.all(margin),
+      padding: EdgeInsets.all(padding),
+      decoration:
+          boxDecoration ?? BoxDecoration(border: Border.all(color: Colors.red)),
+      child: this,
+    );
+  }
+}
