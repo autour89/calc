8b827a3e423b448157f8d0d43bf7bd29e3dba6ed
Oleksandr Shastun <Oleksandr.Shastun@3shape.com>
2021-03-08 00:07:03 +0200

    updated calc face

--- lib/screens/home_vscreen.dart
+++ lib/screens/home_vscreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quiver/iterables.dart';
 import '../blocs/home_bloc.dart';
+import '../screens/styles/extensions.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -14,44 +16,54 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var bloc = context.read<HomeBloc>();
     return Scaffold(
-        appBar: AppBar(),
+        backgroundColor: Colors.grey[310],
         body: SafeArea(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-              SizedBox(
-                  width: double.infinity,
-                  height: 100,
-                  child: Container(
-                      margin: const EdgeInsets.all(8),
-                      padding: const EdgeInsets.all(8),
-                      child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            context.watch<HomeBloc>().outputText,
-                            style: TextStyle(fontSize: 22),
-                          )),
-                      color: Colors.teal[100])),
-              Expanded(
-                  child: GridView.count(
-                      crossAxisCount: 3,
-                      padding: EdgeInsets.all(10),
-                      primary: false,
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      children: [
-                    ...bloc.models.map((m) => Container(
-                        padding: const EdgeInsets.all(8),
-                        child: TextButton(
-                          child: Text(
-                            m.value,
-                            style: TextStyle(fontSize: 26),
-                          ),
-                          onPressed: () => bloc.calc(model: m),
+            child: Column(children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Align(
+                      alignment: Alignment.centerRight,
+                      child: FittedBox(
+                        child: Text(
+                          context.watch<HomeBloc>().outputText,
+                          style: TextStyle(fontSize: 30),
                         ),
-                        color: Colors.teal[100]))
-                  ]))
-            ])));
+                      )),
+                  color: Colors.transparent)),
+          Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  ...partition(bloc.models, 3).map((e) => Expanded(
+                        child: Row(
+                          children: [
+                            ...e.map(
+                              (e) => Expanded(
+                                  child: Container(
+                                margin: EdgeInsets.all(3),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.grey[700],
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30))),
+                                  onPressed: () => bloc.calc(model: e),
+                                  child: Center(
+                                    child: Text(
+                                      e.value,
+                                      style: TextStyle(fontSize: 26),
+                                    ),
+                                  ),
+                                ),
+                              )),
+                            )
+                          ],
+                        ),
+                      ))
+                ],
+              ))
+        ])));
   }
 }
